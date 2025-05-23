e3ef2264691047856d461b9119848ed064063ea3
Stephen-J4455 <164698827+Stephen-J4455@users.noreply.github.com>
2025-05-23 02:52:40 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -11,15 +11,14 @@ import wikipedia
 
 def load_model():
     try:
-        nlp = spacy.load("en_core_web_sm")
+        return spacy.load("en_core_web_sm")
     except OSError:
         from spacy.cli import download
         download("en_core_web_sm")
         return spacy.load("en_core_web_sm")
 
+
 nlp=load_model()
-
-
 
 # Supabase Setup
 url = "https://sffgznknlmqxtikkyhwu.supabase.co"
